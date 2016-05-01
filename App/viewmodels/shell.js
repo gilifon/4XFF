2983334a54ce136590bba16dc34d29cc3e78916f
gilifon <gilifon@gmail.com>
2016-05-01 07:10:20 +0300

    First Checkin

---
+++ App/viewmodels/shell.js
@@ -0,0 +1,37 @@
+﻿define(['plugins/router', 'durandal/app'], function (router, app) {
+
+    
+    var selectedSubMenu = ko.observable('');
+    var selectedMainMenu = ko.observable('main');
+    var version = app.version;
+    var lang = ko.observable('en');
+
+    this.toggleLanguage = function()
+    {
+        if (lang() == 'en') {
+            lang('he');
+        }
+        else if (lang() == 'he') {
+            lang('en');
+        }
+    }
+    
+    return {
+        selectedSubMenu: selectedSubMenu,
+        selectedMainMenu: selectedMainMenu,
+        version: version,
+        router: router,
+        lang:lang,
+        activate: function () {
+            router.map([
+                { route: 'Dashboard', title: 'Dashboard', moduleId: 'viewmodels/dashboard', nav: true },
+                { route: '', title: 'Sections', moduleId: 'viewmodels/sections', nav: true }
+            ]).buildNavigationModel();
+
+            return router.activate();
+        },
+        compositionComplete: function () {
+            
+        }
+    };
+});
